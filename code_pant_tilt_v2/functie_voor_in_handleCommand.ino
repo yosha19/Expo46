a17c8496ebd068bf44890d403f99adcc256c405e
yosha19 <80766533+yosha19@users.noreply.github.com>
2021-04-21 19:10:21 +0200

    Update functie_voor_in_handleCommand.ino

--- code_pant_tilt_v2/functie_voor_in_handleCommand.ino
+++ code_pant_tilt_v2/functie_voor_in_handleCommand.ino
@@ -1,39 +1,4 @@
 
-float panDegreesToSteps(float angle) {
-  return pan_steps_per_degree * angle;
-}
-
-float tiltDegreesToSteps(float angle) {
-  return tilt_steps_per_degree * angle;
-}
-
-/*void panHome(){
-
-      //panStepper.currentPosition();
-      float target = panDegreesToSteps(-90);
-      panStepper.moveTo(target);
-    if (digitalRead(PIN_PAN_HALL) == 0) {
-      Serial.println(PIN_PAN_HALL);
-      panStepper.setCurrentPosition(0);//set step count to 0
-      panStepper.moveTo(0);
-      panHomeFlag = true;
-      panHomingDir = 1;
-    }
-  }
-*/  
-/*void tiltHome(){
-
-    tiltStepper.currentPosition();
-    long target = tiltDegreesToSteps(-90);
-    tiltStepper.moveTo(target);
-    if (digitalRead(PIN_TILT_HALL) == 0) {
-      tiltStepper.setCurrentPosition(0);//set step count to 0
-      tiltStepper.moveTo(0);
-      tiltHomeFlag = true;
-      tiltHomingDir = 1;
-    }
-  }
-*/  
 
 float panStepsToDegrees(long steps) {
   return steps / pan_steps_per_degree;
@@ -49,4 +14,65 @@ float tiltStepsToDegrees(long steps) {
 
 float tiltStepsToDegrees(float steps) {
   return steps / tilt_steps_per_degree;
+}
+float panDegreesToSteps(float angle) {
+  return pan_steps_per_degree * angle;
+}
+
+float tiltDegreesToSteps(float angle) {
+  return tilt_steps_per_degree * angle;
+}
+
+void panHome() {
+  long target = panDegreesToSteps(360);
+  long p = panDegreesToSteps(12.0);
+  panStepper.moveTo(target);
+  while ( digitalRead(PIN_PAN_HALL) != 0){
+    Serial.println("pah runnen");
+    panStepper.run();
+  }
+  Serial.println(PIN_PAN_HALL);
+  Serial.println("pah stop");
+  panStepper.setCurrentPosition(0);//set step count to 0
+  panStepper.moveTo(0);
+  panHomeFlag = true;
+  panHomingDir = 1;
+}
+
+void homeStepper(AccelStepper& stepper, uint8_t hallPin, float offset, long maxSteps) {
+  stepper.move(maxSteps);
+
+  // Wait until we don't see the homing magnet (probably immediately)
+  while (digitalRead(hallPin) == 0 && stepper.run());
+
+  // Wait until we see the homing magnet (should take up to one rotation
+  while ( digitalRead(hallPin) != 0 && stepper.run());
+  long hallDetectionBegin = stepper.currentPosition();
+
+  while (digitalRead(hallPin) == 0 && stepper.run());
+  long hallDetectionEnd = stepper.currentPosition();
+  long hallDetectionMiddle = (hallDetectionBegin + hallDetectionEnd) / 2;
+
+  Serial.println("tah stop");
+//  stepper.runToNewPosition(stepper.currentPosition() + tiltDegreesToSteps(offset));
+  stepper.runToNewPosition(hallDetectionMiddle + tiltDegreesToSteps(offset));
+//  while (tiltStepper.run());
+  stepper.setCurrentPosition(0);//set step count to 0
+//  tiltHomeFlag = true;
+//  tiltHomingDir = 1;
+  
+}
+
+void tiltHome() {
+  long t = tiltDegreesToSteps(12.0);
+  long target = tiltDegreesToSteps(360);
+  tiltStepper.moveTo(target);
+  while ( digitalRead(PIN_TILT_HALL) != 0 && tiltStepper.run());
+  Serial.println("tah stop");
+  tiltStepper.runToNewPosition(tiltStepper.currentPosition() + t);
+//  while (tiltStepper.run());
+  tiltStepper.setCurrentPosition(0);//set step count to 0
+  tiltHomeFlag = true;
+  tiltHomingDir = 1;
+  
 }
