62fded33dc898f2275a8a673d7989fa0a5503e52
Nielsvhb99 <81251659+Nielsvhb99@users.noreply.github.com>
2021-04-21 08:52:59 +0200

    Add files via upload

--- code_pant_tilt_v2/code_pant_tilt_v2.ino
+++ code_pant_tilt_v2/code_pant_tilt_v2.ino
@@ -3,10 +3,10 @@
 
 //Pin definitions when using Arduino Nano pinout
 #define BAUD_RATE 9600
-#define PIN_PAN_HALL A3
-#define PIN_TILT_HALL A4
+#define PIN_PAN_HALL A6
+#define PIN_TILT_HALL A7
 #define PIN_INPUT_VOLTAGE A5
-#define PIN_ENABLE 8 
+#define PIN_ENABLE 8
 #define PIN_MS1 11
 #define PIN_MS2 10
 #define PIN_STEP_TILT 6
@@ -14,29 +14,33 @@
 #define PIN_STEP_PAN 7
 #define PIN_DIRECTION_PAN 4
 
-#define HALF_STEP 2
+#define HALF_STEP 2 
 #define QUARTER_STEP 4
 #define EIGHTH_STEP 8
 #define SIXTEENTH_STEP 16
 
 #define PAN_GEAR_RATIO 144/17 //44/17  // teeth
-//Used with the belt driven tilt axis.
 #define TILT_GEAR_RATIO 64/21 // teeth
 
-AccelStepper panStepper= AccelStepper(1, PIN_STEP_PAN, PIN_DIRECTION_PAN);
-AccelStepper tiltStepper= AccelStepper(1, PIN_STEP_TILT, PIN_DIRECTION_TILT);
+AccelStepper panStepper = AccelStepper(1, PIN_STEP_PAN, PIN_DIRECTION_PAN);
+AccelStepper tiltStepper = AccelStepper(1, PIN_STEP_TILT, PIN_DIRECTION_TILT);
 
 
-float pan_steps_per_degree = (200.0 * 8 * PAN_GEAR_RATIO) / 360.0; //Stepper motor has 200 steps per 360 degrees
-float tilt_steps_per_degree = (200.0 * 8 * TILT_GEAR_RATIO) / 360.0; //Stepper motor has 200 steps per 360 degrees
-float pan_max_speed = 18; //degrees/second. 
+float pan_steps_per_degree = (200.0 * 1 * PAN_GEAR_RATIO) / 360.0 / 2; //Stepper motor has 200 steps per 360 degrees, dus 200/360 geeft aantal stappen per graad, deel je dit nog eens door 2 heb je het aantal stappen per halve graad. 
+                                                                       //(begin regel hierboven met lezen) Het kleine tandwiel moet sneller draaien dan het grote tandwiel, waarvoor de verhouding van 144/17 geldt. 
+                                                                       //Maar andersom gekeken zal het grote tandwiel dus langzamer moeten draaien dus geldt de verhouding van 17/144.
+                                                                       //De motor stuurt het kleine tandwiel aan waardoor de verhouding van 144/17 geldt. Ditzelfde principe geldt voor de motor voor tilting.
+float tilt_steps_per_degree = (200.0 * 1 * TILT_GEAR_RATIO) / 360.0 / 2; //Stepper motor has 200 steps per 360 degrees
+float pan_max_speed = 18; //degrees/second. Deze kan dus aangepast worden naar hoelang de beweging mag duren. De eis was om binnen 4 sec 45 graden te halen, en dat haal je hier wel mee.
 float tilt_max_speed = 10; //degrees/second.
-//int step_mode = SIXTEENTH_STEP;
-float hall_pan_offset_degrees = 0; //Offset to make the pan axis home position centred. This is required because the Hall sensor triggers before being centred on the magnet.
-float hall_tilt_offset_degrees = 0; //Offset to make the tilt axis home position centred. This is required because the Hall sensor triggers before being centred on the magnet.
+bool panHomeFlag = false;
+bool tiltHomeFlag = false;
+int panHomingDir = -1;
+int tiltHomingDir = -1;
+int hallState1 = 0;
+int hallState2 = 0;
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(BAUD_RATE);
   pinMode(PIN_MS1, OUTPUT);
   pinMode(PIN_MS2, OUTPUT);
@@ -45,7 +49,7 @@ void setup() {
   pinMode(PIN_STEP_PAN, OUTPUT);
   pinMode(PIN_DIRECTION_TILT, OUTPUT);
   pinMode(PIN_STEP_TILT, OUTPUT);
-  pinMode(PIN_PAN_HALL, INPUT_PULLUP);
+  pinMode(PIN_PAN_HALL, INPUT);
   pinMode(PIN_TILT_HALL, INPUT_PULLUP);
 
   panStepper.setAcceleration(5000);
@@ -57,6 +61,7 @@ void setup() {
   //digitalWrite(PIN_MS2, HIGH); //Enable the stepper drivers
   //PORTB |= B00001100;
 }
+
 
 // key=value => value
 // Assumes that line does not contain line endings
@@ -77,37 +82,25 @@ float testioGetFloat(String line) {
 }
 
 void handleCommand(String command) {
-  if (command.startsWith("p=")){
-    float target = panDegreesToSteps(testioGetFloat(command));
+  if (command.startsWith("p_target=")) {
+    long target = panDegreesToSteps(testioGetFloat(command));
     panStepper.moveTo(target);
-    Serial.print("p=");
-    Serial.println(target);
- 
-  } else if (command.startsWith("tnewpos=")) {
-    float target = tiltDegreesToSteps(testioGetFloat(command));
-    tiltStepper.moveTo(target);
-    Serial.print("tnewpos=");
+    Serial.print("p_target=");
     Serial.println(target);
 
   } else if (command == "p_target?") {
     Serial.print("p_target=");
     Serial.println(panStepsToDegrees(panStepper.targetPosition()));
 
+  } else if (command.startsWith("t_target=")) {
+    long target = tiltDegreesToSteps(testioGetFloat(command));
+    tiltStepper.moveTo(target);
+    Serial.print("t_target=");
+    Serial.println(target);
+
   } else if (command == "t_target?") {
     Serial.print("t_target=");
     Serial.println(tiltStepsToDegrees(tiltStepper.targetPosition()));
-
-    } else if (command.startsWith("ppos=")) {
-    float pos = panDegreesToSteps(testioGetFloat(command));
-    panStepper.setCurrentPosition(pos);
-    Serial.print("z_pos=");
-    Serial.println(pos);
-
-    } else if (command.startsWith("tpos=")) {
-    float pos = tiltDegreesToSteps(testioGetFloat(command));
-    tiltStepper.setCurrentPosition(pos);
-    Serial.print("tpos=");
-    Serial.println(pos);
 
   } else if (command == "p_pos?") {
     Serial.print("p_pos=");
@@ -122,6 +115,19 @@ void handleCommand(String command) {
     Serial.println(panStepsToDegrees(panStepper.currentPosition()));
     Serial.print("t_pos=");
     Serial.println(tiltStepsToDegrees(tiltStepper.currentPosition()));
+
+  } else if (command == "p_stop") {
+    panStepper.stop();
+    Serial.print("p_stop");
+
+  } else if (command == "t_stop") {
+    tiltStepper.stop();
+    Serial.print("t_stop");
+
+  } else if (command == "stop") {
+    panStepper.stop();
+    tiltStepper.stop();
+    Serial.print("stop");
 
   } else if (command.startsWith("p_accel=")) {
     float accel = panDegreesToSteps(testioGetFloat(command));
@@ -167,16 +173,62 @@ void handleCommand(String command) {
   } else if (command == "p_enable?") {
     Serial.print("p_enable=");
     Serial.println(!digitalRead(PIN_ENABLE));
-    
+
   } else if (command == "panHome") {
-    // panHome();
-    Serial.print("panHome");
+      //panStepper.moveTo(-100);
+      hallState1 = digitalRead(PIN_PAN_HALL);
+      while (panStepper.run()) {
+    if (hallState1 == 0) {
+      panStepper.setCurrentPosition(0);//set step count to 0
+      //panStepper.moveTo(0);
+      panHomeFlag = true;
+      panHomingDir = 1;
+    }
+      Serial.println(hallState1);
+      Serial.println("panHome");
+      //Serial.println(panHomeFlag);
+      }
+      
+  } else if (command == "panHome?") {
+    Serial.print("panHome=");
+    Serial.println(panHomeFlag);
 
   } else if (command == "tilthome") {
-    // tiltHome();
+    //tiltHome();
+    //tiltStepper.moveTo(-100);
+      hallState2 = digitalRead(PIN_TILT_HALL);
+      while (tiltStepper.run()) {
+    if (hallState2 == 0) {
+      tiltStepper.setCurrentPosition(0);//set step count to 0
+      //panStepper.moveTo(0);
+      tiltHomeFlag = true;
+      tiltHomingDir = 1;
+    }
     Serial.print("tiltHome");
+      }
+  } else if (command == "tiltHome?") {
+    Serial.print("tiltHome=");
+    Serial.println(tiltHomeFlag);
 
-    
+  } else if (command == "?") {
+    Serial.println("p_target=");
+    Serial.println(panStepsToDegrees(panStepper.targetPosition()));
+    Serial.println("t_target=");
+    Serial.println(tiltStepsToDegrees(tiltStepper.targetPosition()));
+    Serial.println("p_pos=");
+    Serial.println(panStepsToDegrees(panStepper.currentPosition()));
+    Serial.println("t_pos=");
+    Serial.println(tiltStepsToDegrees(tiltStepper.currentPosition()));
+    Serial.println("p_max_speed=");
+    Serial.println(panStepsToDegrees(panStepper.maxSpeed()));
+    Serial.println("t_max_speed=");
+    Serial.println(tiltStepsToDegrees(tiltStepper.maxSpeed()));
+    Serial.println("p_enable=");
+    Serial.println(!digitalRead(PIN_ENABLE));
+    Serial.println("panHome=");
+    Serial.println(panHomeFlag);
+    Serial.println("tiltHome=");
+    Serial.println(tiltHomeFlag);
 
   } else {
     Serial.print("Unknown command: ");
@@ -187,14 +239,17 @@ void handleCommand(String command) {
 
 String command;
 void loop() {
-  
+
   if (Serial.available()) {
     char c = Serial.read();
     if (c != '\r' && c != '\n') {
       command += c;
-    } else {
-      handleCommand(command);
-      command = "";
+    } 
+    else {
+    if(command != ""){
+      handleCommand(command); 
+    }
+    command = "";
     }
   }
 
--- code_pant_tilt_v2/functie_voor_in_handleCommand.ino
+++ code_pant_tilt_v2/functie_voor_in_handleCommand.ino
@@ -8,114 +8,33 @@ float tiltDegreesToSteps(float angle) {
 }
 
 /*void panHome(){
-    while (digitalRead(PIN_PAN_HALL) == 0) { //If already on a Hall sensor move off
-      target_position[0] = target_position[0] + panDegreesToSteps(!digitalRead(PIN_PAN_HALL));//increment by 1 degree
-      if (target_position[0] > panDegreesToSteps(360)) { //If both axis have done more than a full rotation there must be an issue...
-        return false;
-      }
-      panStepper.moveTo(target_position[0]);
-      panStepper.runSpeedToPosition();
-    }
-    panStepper.setCurrentPosition(0);//set step count to 0
-}
-*/
-/*void tiltHome(){
-    while (digitalRead(PIN_TILT_HALL) == 0) { //If already on a Hall sensor move off
-      target_position[1] = target_position[1] + tiltDegreesToSteps(!digitalRead(PIN_TILT_HALL));//increment by 1 degree
-      if (target_position[1] > tiltDegreesToSteps(360)) { //If both axis have done more than a full rotation there must be an issue...
-        return false;
-      }
-      stepper_tilt.moveTo(target_position[1]);
-      stepper_tilt.runSpeedToPosition();
-    }
-    stepper_tilt.setCurrentPosition(0);//set step count to 0
-}
-*/
-/*bool findHome(void) {
-  bool panHomeFlag = false;
-  bool tiltHomeFlag = false;
-  int panHomingDir = -1;
-  int tiltHomingDir = -1;
 
-  target_position[0] = stepper_pan.currentPosition();
-  target_position[1] = stepper_tilt.currentPosition();
-
-  if (homing_mode == 0) { //No homing
-    return false;
-  }
-
-  if (homing_mode == 1 || homing_mode == 3) { //Home pan
-    while (digitalRead(PIN_PAN_HALL) == 0) { //If already on a Hall sensor move off
-      target_position[0] = target_position[0] + panDegreesToSteps(!digitalRead(PIN_PAN_HALL));//increment by 1 degree
-      if (target_position[0] > panDegreesToSteps(360)) { //If both axis have done more than a full rotation there must be an issue...
-        return false;
-      }
-      stepper_pan.moveTo(target_position[0]);
-      stepper_pan.runSpeedToPosition();
-    }
-    stepper_pan.setCurrentPosition(0);//set step count to 0
-  }
-
-  if (homing_mode == 2 || homing_mode == 3) { //Home tilt
-    while (digitalRead(PIN_TILT_HALL) == 0) { //If already on a Hall sensor move off
-      target_position[1] = target_position[1] + tiltDegreesToSteps(!digitalRead(PIN_TILT_HALL));//increment by 1 degree
-      if (target_position[1] > tiltDegreesToSteps(360)) { //If both axis have done more than a full rotation there must be an issue...
-        return false;
-      }
-      stepper_tilt.moveTo(target_position[1]);
-      stepper_tilt.runSpeedToPosition();
-    }
-    stepper_tilt.setCurrentPosition(0);//set step count to 0
-  }
-
-  setTargetPositions(-45, -45);
-  while (multi_stepper.run()) {
+      //panStepper.currentPosition();
+      float target = panDegreesToSteps(-90);
+      panStepper.moveTo(target);
     if (digitalRead(PIN_PAN_HALL) == 0) {
-      stepper_pan.setCurrentPosition(0);//set step count to 0
-      setTargetPositions(0, -45 * !tiltHomeFlag);
+      Serial.println(PIN_PAN_HALL);
+      panStepper.setCurrentPosition(0);//set step count to 0
+      panStepper.moveTo(0);
       panHomeFlag = true;
       panHomingDir = 1;
     }
+  }
+*/  
+/*void tiltHome(){
+
+    tiltStepper.currentPosition();
+    long target = tiltDegreesToSteps(-90);
+    tiltStepper.moveTo(target);
     if (digitalRead(PIN_TILT_HALL) == 0) {
-      stepper_tilt.setCurrentPosition(0);
-      setTargetPositions(-45 * !panHomeFlag, 0);
+      tiltStepper.setCurrentPosition(0);//set step count to 0
+      tiltStepper.moveTo(0);
       tiltHomeFlag = true;
       tiltHomingDir = 1;
     }
   }
+*/  
 
-  setTargetPositions(360 * !panHomeFlag, 360 * !tiltHomeFlag);//full rotation on both axis so it must pass the home position
-  while (multi_stepper.run()) {
-    if (digitalRead(PIN_PAN_HALL) == 0) {
-      stepper_pan.setCurrentPosition(0);//set step count to 0
-      setTargetPositions(0, 360 * !tiltHomeFlag);
-      panHomeFlag = true;
-    }
-    if (digitalRead(PIN_TILT_HALL)  == 0) {
-      stepper_tilt.setCurrentPosition(0);
-      setTargetPositions(360 * !panHomeFlag, 0);
-      tiltHomeFlag = true;
-    }
-  }
-
-  if (panHomeFlag && tiltHomeFlag) {
-    setTargetPositions(hall_pan_offset_degrees * panHomingDir, hall_tilt_offset_degrees * tiltHomingDir);
-    multi_stepper.runSpeedToPosition();
-    stepper_pan.setCurrentPosition(0);//set step count to 0
-    stepper_tilt.setCurrentPosition(0);//set step count to 0
-    setTargetPositions(0, 0);
-    if (homing_mode == 3) {
-      return false;
-    }
-    else {
-      return true;
-    }
-  }
-  else {
-    return false;
-  }
-}
-*/
 float panStepsToDegrees(long steps) {
   return steps / pan_steps_per_degree;
 }
