cb002b3d2ccb3e9e854bc31bd68cedafb4bef4a2
yosha19 <80766533+yosha19@users.noreply.github.com>
2021-06-01 11:21:59 +0200

    Update code_pant_tilt_v2.ino

--- code_pant_tilt_v2/code_pant_tilt_v2.ino
+++ code_pant_tilt_v2/code_pant_tilt_v2.ino
@@ -1,38 +1,41 @@
 #include <AccelStepper.h>
-
-#define Y_nEN  (24)      //Or A0 for analog (a7)
-#define Y_OPTO  (25)    //Or A1 for analog (a6)
-#define THERM (26)      //Or A2 for analog (a5)
-#define Z_STEP  (27)    //Or A3 for analog (a4)
-#define Z_DIR  (28)     //Or A4 for analog (a3)
-#define Z_nEN  (29)      //Or A5 for analog (a2)
-#define Z_OPTO  (30)    //Or A6 for analog (a1)
-#define E_STEP  (4)     //b4
-#define E_nEN  (3)       //b3
-#define E_DIR  (2)      //b2
-#define Y_STEP  (23)    //c7
-#define Y_DIR (22)      //c6
-#define X_OPTO  (20)    //c4
-#define X_nEN  (19)      //c3
-#define X_DIR  (18)     //c2
-#define SDA  (17)       //c1
-#define SCL  (16)       //c0
-#define X_STEP  (15)    //d7
-#define HEAT  (14)      //d6
+int count = 1;
+int array[] = {0};
+int count2 = 1;
+int array2[] = {0};
+//#define Y_nEN  (24)      //Or A0 for analog (a7)
+//#define Y_OPTO  (25)    //Or A1 for analog (a6)
+//#define THERM (26)      //Or A2 for analog (a5)
+//#define Z_STEP  (27)    //Or A3 for analog (a4)
+//#define Z_DIR  (28)     //Or A4 for analog (a3)
+//#define Z_nEN  (29)      //Or A5 for analog (a2)
+//#define Z_OPTO  (30)    //Or A6 for analog (a1)
+//#define E_STEP  (4)     //b4
+//#define E_nEN  (3)       //b3
+//#define E_DIR  (2)      //b2
+//#define Y_STEP  (23)    //c7
+//#define Y_DIR (22)      //c6
+//#define X_OPTO  (20)    //c4
+//#define X_nEN  (19)      //c3
+//#define X_DIR  (18)     //c2
+//#define SDA  (17)       //c1
+//#define SCL  (16)       //c0
+//#define X_STEP  (15)    //d7
+//#define HEAT  (14)      //d6
 
 
 //Pin definitions when using Arduino Nano pinout
 #define BAUD_RATE 115200
-#define PIN_PAN_HALL Z_OPTO
-#define PIN_TILT_HALL X_OPTO
-//#define PIN_INPUT_VOLTAGE A5
-//#define PIN_ENABLE 8
-//#define PIN_MS1 11
-//#define PIN_MS2 10
-//#define PIN_STEP_TILT 6
-//#define PIN_DIRECTION_TILT 3
-//#define PIN_STEP_PAN 7
-//#define PIN_DIRECTION_PAN 4
+#define PIN_PAN_HALL  12
+#define PIN_TILT_HALL  13
+#define PIN_INPUT_VOLTAGE A5
+#define PIN_ENABLE 8
+#define PIN_MS1 11
+#define PIN_MS2 10
+#define PIN_STEP_TILT 6
+#define PIN_DIRECTION_TILT 3
+#define PIN_STEP_PAN 7
+#define PIN_DIRECTION_PAN 4
 
 #define HALF_STEP 2
 #define QUARTER_STEP 4
@@ -42,8 +45,8 @@
 #define PAN_GEAR_RATIO (144.0/17.0) //44/17  // teeth
 #define TILT_GEAR_RATIO (64.0/21.0) // teeth
 
-AccelStepper panStepper = AccelStepper(AccelStepper::DRIVER, Z_STEP, Z_DIR);
-AccelStepper tiltStepper = AccelStepper(AccelStepper::DRIVER, X_STEP, X_DIR);
+AccelStepper panStepper = AccelStepper(AccelStepper::DRIVER, PIN_STEP_PAN, PIN_DIRECTION_PAN);
+AccelStepper tiltStepper = AccelStepper(AccelStepper::DRIVER, PIN_STEP_TILT, PIN_DIRECTION_TILT);
 
 
 float pan_steps_per_degree = (400.0 * 8.0 * PAN_GEAR_RATIO) / 360.0 / 2.0; //Stepper motor has 200 steps per 360 degrees, dus 200/360 geeft aantal stappen per graad, deel je dit nog eens door 2 heb je het aantal stappen per halve graad.
@@ -51,8 +54,8 @@ float pan_steps_per_degree = (400.0 * 8.0 * PAN_GEAR_RATIO) / 360.0 / 2.0; //Ste
 //Maar andersom gekeken zal het grote tandwiel dus langzamer moeten draaien dus geldt de verhouding van 17/144.
 //De motor stuurt het kleine tandwiel aan waardoor de verhouding van 144/17 geldt. Ditzelfde principe geldt voor de motor voor tilting.
 float tilt_steps_per_degree = (400.0 * 8.0 * TILT_GEAR_RATIO) / 360.0 / 2.0; //Stepper motor has 200 steps per 360 degrees
-float pan_max_speed = 100; //degrees/second. Deze kan dus aangepast worden naar hoelang de beweging mag duren. De eis was om binnen 4 sec 45 graden te halen, en dat haal je hier wel mee.
-float tilt_max_speed = 100; //degrees/second.
+float pan_max_speed = 200; //degrees/second. Deze kan dus aangepast worden naar hoelang de beweging mag duren. De eis was om binnen 4 sec 45 graden te halen, en dat haal je hier wel mee.
+float tilt_max_speed = 200; //degrees/second.
 bool panHomeFlag = false;
 bool tiltHomeFlag = false;
 int panHomingDir = -1;
@@ -61,29 +64,30 @@ int hallState1 = 0;
 int hallState2 = 0;
 
 void setup() {
-  panStepper.setEnablePin(Z_nEN);
-  tiltStepper.setEnablePin(X_nEN);
+  //panStepper->setEnablePin(Z_nEN);
+  //tiltStepper->setEnablePin(X_nEN);
   panStepper.setPinsInverted( false, false, false, false, true);
   tiltStepper.setPinsInverted( false, false, false, false, true);
-  panStepper.enableOutputs();
-  tiltStepper.enableOutputs();
-  pinMode(X_nEN, OUTPUT);
-  digitalWrite(X_nEN, LOW);
+  //  panStepper->enableOutputs();
+  //  tiltStepper->enableOutputs();
+  //pinMode(X_nEN, OUTPUT);
+  //digitalWrite(X_nEN, LOW);
   Serial.begin(BAUD_RATE);
-  //pinMode(PIN_MS1, OUTPUT);
-  //pinMode(PIN_MS2, OUTPUT);
+  pinMode(PIN_MS1, OUTPUT);
+  pinMode(PIN_MS2, OUTPUT);
   //pinMode(Z_nEN, OUTPUT);
   //pinMode(Y_nEN, OUTPUT);
-  //pinMode(PIN_DIRECTION_PAN, OUTPUT);
-  //pinMode(PIN_STEP_PAN, OUTPUT);
-  //pinMode(PIN_DIRECTION_TILT, OUTPUT);
-  //pinMode(PIN_STEP_TILT, OUTPUT);
-  pinMode(PIN_PAN_HALL, INPUT);
-  pinMode(PIN_TILT_HALL, INPUT);
+  pinMode(PIN_DIRECTION_PAN, OUTPUT);
+  pinMode(PIN_STEP_PAN, OUTPUT);
+  pinMode(PIN_DIRECTION_TILT, OUTPUT);
+  pinMode(PIN_STEP_TILT, OUTPUT);
+  pinMode(PIN_PAN_HALL, INPUT_PULLUP);
+  pinMode(PIN_TILT_HALL, INPUT_PULLUP);
 
-  panStepper.setAcceleration(5000);
+
+  panStepper.setAcceleration(50000);
   panStepper.setMaxSpeed(panDegreesToSteps(pan_max_speed));
-  tiltStepper.setAcceleration(5000);
+  tiltStepper.setAcceleration(50000);
   tiltStepper.setMaxSpeed(tiltDegreesToSteps(tilt_max_speed));
   //digitalWrite(Z_nEN, LOW); //Enable the stepper drivers
   //digitalWrite(Y_nEN, LOW);
@@ -114,9 +118,11 @@ float testioGetFloat(String line) {
 void handleCommand(String command) {
   if (command.startsWith("p_target=")) {
     long target = panDegreesToSteps(testioGetFloat(command));
+    //    array[count] = target1;
+    //    count = count + 1;
     panStepper.moveTo(target);
     Serial.print("p_target=");
-    Serial.println(panStepsToDegrees(target));
+    Serial.println(target);
 
   } else if (command == "p_target?") {
     Serial.print("p_target=");
@@ -124,6 +130,9 @@ void handleCommand(String command) {
 
   } else if (command.startsWith("t_target=")) {
     long target = tiltDegreesToSteps(testioGetFloat(command));
+    //    array2[count2] = target2;
+    //    count2 = count2 + 1;
+
     tiltStepper.moveTo(target);
     Serial.print("t_target=");
     Serial.println(target);
@@ -203,7 +212,7 @@ void handleCommand(String command) {
 
   } else if (command == "autohome?") {
     //panHome();
-//    tiltHome();
+    //tiltHome();
     homeStepper(panStepper, PIN_PAN_HALL, 0.0, panDegreesToSteps(720.0));
     homeStepper(tiltStepper, PIN_TILT_HALL, -5.6, tiltDegreesToSteps(720.0));
     Serial.println("autohome=1");
@@ -229,13 +238,22 @@ void handleCommand(String command) {
     Serial.println("tiltHome=");
     Serial.println(tiltHomeFlag);
 
-  } else {
-    Serial.print("Unknown command: ");
-    Serial.println(command);
-
   }
+  //   else if (command == ("start")) {
+  //    for (int i = 0; i <= count; i++) {
+  //      panStepper->moveTo(array[i]);
+  //    }
+  //    for (int i = 0; i <= count2; i++) {
+  //      tiltStepper->moveTo(array2[i]);
+  //
+  //    }
+  //  }else {
+  //    Serial.print("Unknown command: ");
+  //    Serial.println(command);
+  //
+  //  }
+  //}
 }
-
 String command;
 void loop() {
 
@@ -251,10 +269,15 @@ void loop() {
       command = "";
     }
   }
-//  Serial.print("digitalRead(PIN_PAN_HALL)=");
-//Serial.print(digitalRead(PIN_PAN_HALL));
-//  Serial.print(", digitalRead(PIN_TILT_HALL)=");
-//Serial.println(digitalRead(PIN_TILT_HALL));
-  panStepper.run();
-  tiltStepper.run();
+  Serial.print("digitalRead(PIN_PAN_HALL)=");
+  Serial.print(digitalRead(PIN_PAN_HALL));
+  Serial.print(", digitalRead(PIN_TILT_HALL)=");
+  Serial.println(digitalRead(PIN_TILT_HALL));
+    panStepper.run();
+    tiltStepper.run();
+//  pinMode(PIN_PAN_HALL, INPUT);
+//  pinMode(PIN_TILT_HALL, INPUT);
+
+
+
 }
